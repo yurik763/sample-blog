e1f69c9199aadd51d212b1628c1ad39a799fb30b
yurik763 <yurik763@tut.by>
2019-06-10 22:18:32 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
