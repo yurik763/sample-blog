0c866adce5bb7f38ad8a3739009fb97912710933
yurik763 <yurik763@tut.by>
2019-06-15 15:18:03 +0300

    Update articles create

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resource :about, only: [:new, :create], path_names: { :new => ''}
   resource :terms, only: [:new, :create], path_names: { :new => ''}
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
-  resources :articles, only: [:new, :create], path_names: { :new => ''}
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
