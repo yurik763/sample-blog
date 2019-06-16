0373aad428160ed49f6743db8f3388f28c218912
yurik763 <yurik763@tut.by>
2019-06-16 11:41:44 +0300

    Update articles#destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,10 +36,8 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to @article
+    redirect_to articles_path
    end
-
-
 
   private
 
