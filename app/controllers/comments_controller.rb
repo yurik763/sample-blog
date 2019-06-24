0886a51e5aadd5f3c7911617bd4c4fd80d92b58e
yurik763 <yurik763@tut.by>
2019-06-24 23:27:27 +0300

    Update create comments, name author  inserted automatically

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,10 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
   def create
-    @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+     @article = Article.find(params[:article_id])
+     @c = @article.comments.new(comment_params)
+     @c.author = current_user.username
+     @c.save
 
     redirect_to article_path(@article)
   end
