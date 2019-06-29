59efce7ae3bbea70e13ad0ccd24e71bf4a67846a
yurik763 <yurik763@tut.by>
2019-06-30 00:24:39 +0300

    Update comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Comment do
+  describe "validations" do
   it { should belong_to :article }
-
+  it { should validate_presence_of :body }
+  it { should validate_length_of(:body).is_at_most(4000) }
+  end
 end
