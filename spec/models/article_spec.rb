141b7f7f279e9a3690ebd426d694e293c50b3701
yurik763 <yurik763@tut.by>
2019-06-29 15:01:05 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,8 +5,19 @@ describe Article do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
   end
-  
+
   describe "associations" do
     it { should have_many :comments }
+  end
+
+
+  describe "#subject" do
+    it "returns the article title" do
+      # создаем объект article
+      article = create(:article, title: 'Lorem')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem'
+    end
   end
 end
