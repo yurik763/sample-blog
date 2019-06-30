68ebd7b9b7a30ce16ce5577c2864e8c36db0b044
yurik763 <yurik763@tut.by>
2019-06-30 11:39:34 +0300

    Update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,6 @@ feature "Comtact Creation" do
   scenario "allows acees to contacts page" do
     visit new_contacts_path
 
-    expect(page).to have_content 'Контакты'
+    expect(page).to have_content  I18n.t('contacts.contact_us')
   end  
 end
