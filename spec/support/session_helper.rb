eaa18525993f429586631ad7296ecf789b0331f2
yurik763 <yurik763@tut.by>
2019-06-30 13:17:47 +0300

    Refactoring: move sign up to session_helper.rb

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,12 +1,3 @@
-require "rails_helper"
-
-feature "Account Creation" do 
-  scenario "allows guest to create account" do
-    sign_up
-    expect(page).to have_content I18n.t('devise.registrations.signed_up')
-  end 
-end
-
 def sign_up
     visit new_user_registration_path
 
