fdfb6a0f4aa445b25b57707cf2d9928908040592
yurik763 <yurik763@tut.by>
2019-06-30 16:19:48 +0300

    Add spec for article

---
+++ spec/features/user_create_article_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+require "support/session_helper.rb"
+require "support/database_cleaner.rb"
+
+feature "Article Creation" do
+  before(:each) do
+    sign_up
+  end
+
+  scenario "allows user to visit new article page" do
+    visit new_article_path
+    expect(page).to have_content 'New article'
+  end 
+end
